a457102955167402371f007be086a7751fd09a95
Thom Lamb <fitnessevangelist@gmail.com>
2020-07-11 17:32:28 -0400

    Adding env file to repo so people can build it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import Board from 'react-trello';
 import {auth, db} from './Firebase'
 import './App.css';
 
+//TODO set up firebase realtime or cloud firestore. DONE
+//move data to firebase before anything else and get it into a good structure. 
+//cloud function that can be called only by admin to retrieve a json or a csv. 
+// get a good name and url 
+//TODO prompt person to login with google auth 
+//TODO export to a json that is easy to work with 
+//TODO landing page - copy format of funretro ? 
+
+//TODO subscribe to db and store cards there 
+//Premium TODO only people with the invite can view or edit the board, after closing the board only the admin can reopen it. 
+// 
+
 const urlParam = '0';
 
 const data = {
@@ -21,8 +33,35 @@ const data = {
       title: 'Completed',
       label: '0/0',
       cards: []
-    }
+    },
+    
   ]
+}
+
+
+//if this is the first time the board has been accessed set up a new default board
+
+function makeBoard(){
+  const data = {
+    lanes: [
+      {
+        id: 'lane1',
+        title: 'Planned Tasks',
+        label: '2/2',
+        cards: [
+          {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
+          {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
+        ]
+      },
+      {
+        id: 'lane2',
+        title: 'Completed',
+        label: '0/0',
+        cards: []
+      },
+    
+  ]};
+  return data;
 }
 
 function App() {
@@ -36,7 +75,7 @@ function App() {
   
   return (
     <div className="App">
-      <Board data={data}/>
+      <Board data={makeBoard()} canAddLanes editable/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { db, auth } from './Firebase'
 
+//TODO set up firebase realtime or cloud firestore. 
+//move data to firebase before anything else and get it into a good structure. 
+//cloud function that can be called only by admin to retrieve a json or a csv. 
+// get a good name and url 
+//TODO prompt person to login with google auth 
+//TODO export to a json that is easy to work with 
+//TODO landing page - copy format of funretro ? 
+
+//TODO subscribe to db and store cards there 
+//Premium TODO only people with the invite can view or edit the board, after closing the board only the admin can reopen it. 
+// 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
