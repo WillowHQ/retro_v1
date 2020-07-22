13f81d728198c90dbc68405153ae80ce01e6d281
Thom Lamb <fitnessevangelist@gmail.com>
2020-07-22 08:26:30 -0400

    Adds loading state

--- pages/AdminDashboard.js
+++ pages/AdminDashboard.js
@@ -1,10 +1,8 @@
 import { db, auth } from '../components/firebase'
 import { useState, useEffect } from 'react'
 
+//Add links
 
-
-
-//now what ? 
 const Board = (props) => <p>Things</p>
 const Boards = ({ boards }) => {
   console.log("What is boards", boards)
@@ -15,9 +13,7 @@ const Boards = ({ boards }) => {
     <ul>{boardContainer}</ul>
   )
 }
-// const numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
-// const listItems = numbers.map((number) =><li>{number}</li>)
-//so the problem is that initially boards is empty and we don't waht to display them until they are retrieved
+const Loading = () => <p>Loading...</p>
 
 export default function AdminDashboard(){
   const [user, setUser] = useState()
@@ -26,6 +22,7 @@ export default function AdminDashboard(){
   const [boards, setBoards] = useState([{title:"thing"}])
 
   //prob need to put all this into a useEffect to keep it cleaner and avoid improper rendering
+
   let adminRef = db.collection('boards')
   auth.onAuthStateChanged(function(user){
     if(user) {
@@ -41,6 +38,7 @@ export default function AdminDashboard(){
   //the next thing I need to do is figure out how to use loading and error to handle the fact that the snapshot query is getting triggered 
 
   useEffect(()=> {
+    setLoading(true)
     if(user){
      //try adding a try catch here ? 
      
@@ -51,6 +49,7 @@ export default function AdminDashboard(){
           querySnapshot.forEach((board) => boardArray.push(board.data().title))
           console.log("Current boards", boardArray.join(", "))
           setBoards(boardArray)
+          setLoading(false)
 
       })
     }
@@ -59,27 +58,18 @@ export default function AdminDashboard(){
   //so I want to add a new board if I click the button. so inside of the admin collection get a reference to that collection 
   // then use that reference to add a new board with an auto generated id, and a dummy title. 
   const addBoard = id => {
-    //check if admin already exists 
-    //actually fuck it just make it flat and don't worry about organizing boards yet
+
     adminRef.add({
       userId: user,
       title: "New Board"
     })
   }
-  //trying to get board to show up, probably doing something silly here 
-  // const Board = (props) => <p>{props.title}</p>
-  // const Boards = () => boards.map((board) => {
-  //   return (
-  //     <Board title={board.title}/>
-  //   )
-  // })
-
 
   return (
     <div>
       <p>This is the admin dashboard</p>
       <Board title="test"/>
-      <Boards boards={boards}/>
+      {loading ? <Loading/> : <Boards boards={boards}/>}
       <button onClick={addBoard}>Now we need a place to make a button</button>
     </div>
 
